94a507fb42679fbbe3efcbb55e7ee5c13fbbc914
James Woglom <j@wogloms.net>
2017-02-19 22:02:58 -0500

    restructure, fix

--- djstart.py
+++ djstart.py
@@ -3,58 +3,8 @@ import argparse
 import os, shutil
 from subprocess import Popen
 
-parser = argparse.ArgumentParser(description='Quick-start a Django project.')
-parser.add_argument('--create-repo',
-                    action='store_true',
-                    dest='create_repo',
-                    help='create a git repo')
-parser.add_argument('--create-venv',
-                    action='store_true',
-                    dest='create_venv',
-                    help='create a virtualenv')
-parser.add_argument('--venv-path',
-                    type=str,
-                    dest='venv_path',
-                    help='outer path to virtualenv environment')
-parser.add_argument('--path',
-                    type=str,
-                    dest='path',
-                    help='outer path (defaults to current directory)')
-parser.add_argument('--python',
-                    type=str,
-                    dest='python',
-                    default='python3',
-                    help='Name of or path to python executable')
-parser.add_argument('name',
-                    metavar='NAME',
-                    type=str,
-                    help='Django project name')
-
-
-args = parser.parse_args()
-
-
-outer_path = args.path or '.'
-outer_path = os.path.abspath(outer_path)
-
-git_path = os.path.join(outer_path, args.name)
-django_path = os.path.join(git_path, args.name)
-
-venv_path = args.venv_path or '~/.virtualenvs'
-venv_path = os.path.join(os.path.expanduser(venv_path), args.name)
-
-python = shutil.which(args.python) or args.python
-
-print("outer_path:", outer_path)
-print("\tgit:", git_path)
-print("\tdjango:", django_path)
-print("venv:", venv_path)
-print("\tpython:", python)
-print("")
-
 def get_venv_exe(exe):
-    global venv_path
-    return os.path.join(venv_path, 'bin', exe)
+    return os.path.abspath(os.path.join(get_venv_exe.PATH, 'bin', exe))
 
 
 def mkdir_repo(git_path):
@@ -69,29 +19,99 @@ def create_repo(git_path):
         Popen(['git', 'init', git_path]).wait()
 
 def create_venv(venv_path, python):
-    Popen(['virtualenv', '--python={}'.format(python), venv_path]).wait()
+    if os.path.exists(venv_path):
+        print("Virtualenv exists, ignoring.")
+    else:
+        Popen(['virtualenv', '--python={}'.format(python), venv_path]).wait()
 
-def pip_generate_requirements(git_path, pip_reqs):
+def pip_generate_requirements(git_path, pip_reqs=[]):
     reqpath = os.path.join(git_path, 'requirements.txt')
-    with open(reqpath, 'a') as fo:
+    with open(reqpath, 'w') as fo:
         fo.write('\n'.join(pip_reqs))
-    
+        fo.write('\n')
+    fo.close()
 
-def create_project(django_path):
-    Popen([get_venv_exe('django-admin'), 'startproject', django_path])
+def pip_install_requirements(git_path):
+    reqpath = os.path.join(git_path, 'requirements.txt')
+    Popen([get_venv_exe('pip'), 'install', '-r{}'.format(reqpath)]).wait()
 
-
-mkdir_repo(git_path)
-if args.create_repo:
-    create_repo(git_path)
-
-if args.create_venv:
-    create_venv(venv_path, python)
-
-pip_generate_requirements(git_path, pip_reqs)
+def django_create_project(git_path, name):
+    Popen([get_venv_exe('django-admin.py'), 'startproject', name], cwd=git_path).wait()
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Quick-start a Django project.')
+    parser.add_argument('--create-repo',
+                        action='store_true',
+                        dest='create_repo',
+                        help='create a git repo')
+    parser.add_argument('--create-venv',
+                        action='store_true',
+                        dest='create_venv',
+                        help='create a virtualenv')
+    parser.add_argument('--venv-path',
+                        type=str,
+                        dest='venv_path',
+                        help='outer path to virtualenv environment')
+    parser.add_argument('--path',
+                        type=str,
+                        dest='path',
+                        help='outer path (defaults to current directory)')
+    parser.add_argument('--python',
+                        type=str,
+                        dest='python',
+                        default='python3',
+                        help='Name of or path to python executable')
+    parser.add_argument('name',
+                        metavar='NAME',
+                        type=str,
+                        help='Django project name')
 
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    outer_path = args.path or '.'
+    outer_path = os.path.abspath(outer_path)
+
+    git_path = os.path.join(outer_path, args.name)
+    django_path = os.path.join(git_path, args.name)
+
+    venv_path = args.venv_path or '~/.virtualenvs'
+    venv_path = os.path.join(os.path.expanduser(venv_path), args.name)
+    get_venv_exe.PATH = venv_path
+    DJANGO_VERSION = '1.10'
+
+    python = shutil.which(args.python) or args.python
+
+    print("outer_path:", outer_path)
+    print("\tgit:", git_path)
+    print("\tdjango:", django_path)
+    print("venv:", venv_path)
+    print("\tpython:", python)
+
+    mkdir_repo(git_path)
+
+    if args.create_repo:
+        create_repo(git_path)
+
+    if args.create_venv:
+        create_venv(venv_path, python)
+
+    pip_reqs = [
+        'Django~={}'.format(DJANGO_VERSION),
+    ]
+    pip_generate_requirements(git_path, pip_reqs)
+    pip_install_requirements(git_path)
+
+    django_create_project(git_path, args.name)
+
+    #django_warp_project(django_path, args.name)
+
+
+if __name__ == '__main__':
+    main()
 
 #repo/
 #  project/
