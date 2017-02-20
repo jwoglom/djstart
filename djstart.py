8c1b247da56e44ebe815c79119e5a5f065c7b3f0
James Woglom <j@wogloms.net>
2017-02-20 14:52:47 -0500

    move requirements file, fix

--- djstart.py
+++ djstart.py
@@ -6,16 +6,6 @@ from subprocess import Popen
 def get_venv_exe(exe):
     return os.path.abspath(os.path.join(get_venv_exe.PATH, 'bin', exe))
 
-def mkdir_repo(repo_path):
-    if not os.path.exists(repo_path):
-        print("Creating directory:", repo_path)
-        os.mkdir(repo_path)
-
-def create_repo(repo_path):
-    if os.path.exists(os.path.join(repo_path, ".git")):
-        print("Git repository exists, continuing...")
-    else:
-        Popen(['git', 'init', repo_path]).wait()
 
 def create_venv(venv_path, python):
     if os.path.exists(venv_path):
@@ -23,30 +13,37 @@ def create_venv(venv_path, python):
     else:
         Popen(['virtualenv', '--python={}'.format(python), venv_path]).wait()
 
-def pip_generate_requirements(repo_path, pip_reqs=[]):
-    reqpath = os.path.join(repo_path, 'requirements.txt')
+def pip_generate_requirements(outer_path, pip_reqs=[]):
+    reqpath = os.path.join(outer_path, 'requirements.txt')
     with open(reqpath, 'w') as fo:
         fo.write('\n'.join(pip_reqs))
         fo.write('\n')
     fo.close()
 
-def pip_install_requirements(repo_path):
-    reqpath = os.path.join(repo_path, 'requirements.txt')
+def pip_install_requirements(outer_path):
+    reqpath = os.path.join(outer_path, 'requirements.txt')
     Popen([get_venv_exe('pip'), 'install', '-r{}'.format(reqpath)]).wait()
 
-def django_create_project(repo_path, name):
-    if os.path.exists(os.path.join(repo_path, 'manage.py')):
+def django_create_project(outer_path, name):
+    if os.path.exists(os.path.join(outer_path, name, 'manage.py')):
         print("Project exists, continuing...")
     else:
-        Popen([get_venv_exe('django-admin.py'), 'startproject', name], cwd=repo_path).wait()
+        Popen([get_venv_exe('django-admin.py'), 'startproject', name], cwd=outer_path).wait()
 
-def django_move_project(repo_path, name):
-    old_path = os.path.join(repo_path, name)
-    shutil.move(old_path, repo_path)
+def pip_move_requirements(outer_path, name):
+    oldreqs = os.path.join(outer_path, 'requirements.txt')
+    if os.path.exists(oldreqs):
+        shutil.move(oldreqs, os.path.join(outer_path, name, 'requirements.txt'))
 
-def django_create_default_app(django_path, name, app_name='core'):
-    managepy = os.path.join(django_path, 'manage.py')
-    apps = os.path.join(django_path, 'apps')
+def create_repo(repo_path):
+    if os.path.exists(os.path.join(repo_path, ".git")):
+        print("Git repository exists, continuing...")
+    else:
+        Popen(['git', 'init', repo_path]).wait()
+
+def django_create_default_app(repo_path, name, app_name='core'):
+    managepy = os.path.join(repo_path, 'manage.py')
+    apps = os.path.join(repo_path, name, 'apps')
     if not os.path.exists(apps):
         os.mkdir(apps)
     if os.path.exists(os.path.join(apps, app_name)):
@@ -54,18 +51,8 @@ def django_create_default_app(django_path, name, app_name='core'):
     else:
         Popen([managepy, 'startapp', app_name], cwd=apps).wait()
 
-def django_warp_project(django_path, name):
-    
-    """old_path = os.path.join(django_path, name)
-    move_files = ["settings.py", "urls.py", "wsgi.py", "__init__.py"]
-    for i in move_files:
-        frm = os.path.join(old_path, i)
-        to = os.path.join(django_path, i)
-        if os.path.exists(frm):
-            print("mv", frm, to)
-            shutil.move(frm, to)
-    shutil.rmdir(old_path)
-    """
+def django_warp_project(repo_path, name):
+    pass
     
 
 def get_args():
@@ -105,7 +92,7 @@ def main():
     outer_path = os.path.abspath(outer_path)
 
     repo_path = os.path.join(outer_path, args.name)
-    django_path = os.path.join(repo_path, args.name)
+    project_path = os.path.join(repo_path, args.name)
 
     venv_path = args.venv_path or '~/.virtualenvs'
     venv_path = os.path.join(os.path.expanduser(venv_path), args.name)
@@ -116,14 +103,9 @@ def main():
 
     print("outer_path:", outer_path)
     print("\tgit:", repo_path)
-    print("\tdjango:", django_path)
+    print("\tdjango:", project_path)
     print("venv:", venv_path)
     print("\tpython:", python)
-
-    mkdir_repo(repo_path)
-
-    if args.create_repo:
-        create_repo(repo_path)
 
     if args.create_venv:
         create_venv(venv_path, python)
@@ -131,15 +113,17 @@ def main():
     pip_reqs = [
         'Django~={}'.format(DJANGO_VERSION),
     ]
-    pip_generate_requirements(repo_path, pip_reqs)
-    pip_install_requirements(repo_path)
+    pip_generate_requirements(outer_path, pip_reqs)
+    pip_install_requirements(outer_path)
 
-    django_create_project(repo_path, args.name)
-    django_move_project(repo_path, args.name)
+    django_create_project(outer_path, args.name)
 
-    return
-    django_create_default_app(django_path, args.name)
-    django_warp_project(django_path, args.name)
+    pip_move_requirements(outer_path, args.name)
+
+    create_repo(repo_path)
+
+    django_create_default_app(repo_path, args.name)
+    django_warp_project(repo_path, args.name)
 
 
 if __name__ == '__main__':
